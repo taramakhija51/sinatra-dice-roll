609595b4006aa9812af6d3da6ddbdc9e000cfbe3
taramakhija51 <114837329+taramakhija51@users.noreply.github.com>
2024-10-21 05:25:37 +0000

    target reqs

--- dice.rb
+++ dice.rb
@@ -10,41 +10,91 @@ BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 get("/") do
   erb(:elephant)
 end
+
 get("/zebra") do
   "We must add a route for each path we want to support"
 end
+
 get("/giraffe") do
   "Hopefully this shows up without having to restart the server 🤞🏾"
 end
+
+# Route for rolling two 6-sided dice
 get("/dice/2/6") do
   first_die = rand(1..6)
   second_die = rand(1..6)
   sum = first_die + second_die
 	
   @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-  erb(:two_six)
   
- 
+  @rolls = [] # Create an array for random numbers
+  2.times do
+    die = rand(1..6)  # Generate a random number
+    @rolls.push(die)  # Add to the array
+  end
+
+  erb(:two_six)  # Render the template
 end
 
-get ("/dice/2/10") do
-    first_die = rand(1..10)
-    second_die = rand(1..10)
-    sum = first_die + second_die
-    outcome = "you rolled a #{first_die} and a #{second_die} for a total of #{sum}"
-    pp outcome
+# Route for rolling two 10-sided dice
+get("/dice/2/10") do
+  first_die = rand(1..10)
+  second_die = rand(1..10)
+  sum = first_die + second_die
+  
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  
+  @rolls = [] # Create an array for random numbers
+  2.times do
+    die = rand(1..10)  # Generate a random number
+    @rolls.push(die)   # Add to the array
+  end
+
+  erb(:two_ten)
 end
+
+# Route for rolling one 20-sided die
 get("/dice/1/20") do
-  die = rand(1..20)
-  pp "you rolled a #{die}"
+  @die = rand(1..20)
+  @outcome = "You rolled a #{@die}"
+
+  @rolls = [] # Create an array for random numbers
+  1.times do
+    die = rand(1..20)
+    @rolls.push(die)
+  end
+
   erb(:one_twenty)
 end
+
+# Route for rolling five 4-sided dice
 get("/dice/5/4") do
-  die1 = rand(1..4)
-  die2 = rand(1..4)
-  die3 = rand(1..4)
-  die4 = rand(1..4)
-  die5 = rand(1..4)
-  sum = die1 + die2 + die3 + die4 + die5 
-  pp "you rolled a #{die1}, a #{die2}, a #{die3}, a #{die4}, and a #{die5} for a total of #{sum}"
+  first_die = rand(1..4)
+  second_die = rand(1..4)
+  third_die = rand(1..4)
+  fourth_die = rand(1..4)
+  fifth_die = rand(1..4)
+  sum = first_die + second_die + third_die + fourth_die + fifth_die
+	
+  @outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, a #{fourth_die}, and a #{fifth_die} for a total of #{sum}."
+  
+  @rolls = [] # Create an array for random numbers
+  5.times do
+    die = rand(1..4)
+    @rolls.push(die)
+  end
+
+  erb(:five_four)
+end
+
+# Route for rolling 100 6-sided dice
+get("/dice/100/6") do
+  @rolls = [] # Create an array for random numbers
+
+  100.times do
+    die = rand(1..6)
+    @rolls.push(die)
+  end
+
+  erb(:one_hundred_six)
 end
